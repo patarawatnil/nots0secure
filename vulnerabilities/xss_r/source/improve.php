005cdeba3287a0ab053d9e552880fbe1ea4c2440
Patarawat N <p.nilthieach@gmail.com>
2021-04-09 18:14:42 +0700

    Add csrf

--- vulnerabilities/xss_r/source/improve.php
+++ vulnerabilities/xss_r/source/improve.php
@@ -1,7 +1,7 @@
 <?php
 // Is there any input?
 if( isset( $_POST['postarea'] ) ) {
-    // Get input and sanitize which clear all html tags
+    // Get input and sanitize which make all html tags become text not something to render
     $name = htmlspecialchars( $_POST['postarea'] );
     // Feedback for end user
     echo "<p><b>Post</b> <br/>$name</p>";
