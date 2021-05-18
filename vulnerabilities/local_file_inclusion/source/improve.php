138c33cb36553e3b2f1c51ea4e348c2832bfe0c4
Patarawat N <p.nilthieach@gmail.com>
2021-05-18 15:33:41 +0700

    Edit typo

--- vulnerabilities/local_file_inclusion/source/improve.php
+++ vulnerabilities/local_file_inclusion/source/improve.php
@@ -3,7 +3,7 @@
 // The page we wish to display
 $file = $_GET[ 'page' ];
 $found = true;
-// Only allow include.php or file{1..3}.php
+// Only allow file{1..3}.php
 if( $file != "file1.php" && $file != "file2.php" && $file != "file3.php" ) {
     // This isn't the page we want!
     echo "<div>ERROR: File not found!</div>";
--- vulnerabilities/remote_file_inclusion/source/improve.php
+++ vulnerabilities/remote_file_inclusion/source/improve.php
@@ -4,7 +4,7 @@
 $file = $_GET[ 'web' ];
 $found = true;
 // Only allow some website
-if( $file != "https://www.york.ac.uk/teaching/cws/wws/webpage1.html" && $file != "http://wttr.in/" && $file != "https://sjmulder.nl/en/textonly.html" ) {
+if( $file != "https://www.york.ac.uk/teaching/cws/wws/webpage1.html" && $file != "http://wttr.in/") {
     // This isn't the page we want!
     echo "<div>ERROR: Unable Access</div>";
     $found = false;
